6e30654ad9bc183338896ef71417c19c5f375ce7
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-11 18:55:21 +0200

    captura ex. 8

--- covert.js
+++ covert.js
@@ -1,5 +1,5 @@
 function convertirCF() {
-    let CF = prompt("A què vols convertir C / F?");
+    let CF = prompt("A què vols convEEEertir C / F?");
     let temp = parseFloat(prompt("TemperaturTRUKUTRUa:"));
     if( CF.toUpperCase() == "F" ){
         let cent = (temp-32)/(9/5);
